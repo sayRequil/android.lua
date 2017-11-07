e11e9957e49b4b073a2a8f99124a6a18a391f900
Zack P <zack@zacklearns.com>
2017-11-07 16:21:37 -0700

    Create setupInitDirectories.bat

---
+++ windows/setupInitDirectories.bat
@@ -0,0 +1,15 @@
+@echo off
+cd C:/Users/%user%
+mkdir android.lua
+clr
+echo Loaded 25%
+cd android.lua
+mkdir projects
+clr 
+echo Loaded 50%
+mkdir backups
+clr
+echo Loaded 75%
+mkdir windows
+clr
+echo Loaded 100%
